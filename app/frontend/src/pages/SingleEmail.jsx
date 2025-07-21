34a4eba87bbab046ece4ddaffdc609df56b53a5d
Kyle Barth <barthky@oregonstate.edu>
2025-07-21 13:05:02 -0700

    Added tests for MultiEmail, contact pages

--- app/frontend/src/pages/SingleEmail.jsx
+++ app/frontend/src/pages/SingleEmail.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Tabs, Tab, Card } from 'react-bootstrap';
 import './SingleEmail.css';
 
